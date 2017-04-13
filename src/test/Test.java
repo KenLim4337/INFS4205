6124d8479b932fc9e6a66525ae29349136c851b7
James Cullen <jimcullenaus@gmail.com>
2017-04-13 15:19:24 +1000

    Fix createRectangle, fix gitignore

--- src/test/Test.java
+++ src/test/Test.java
@@ -5,10 +5,15 @@ import java.awt.*;
 public class Test {
 	public static void main(String [] args) {
 		System.out.println("Hello, world!");
+		Point p1 = new Point(1, 1);
+		Point p2 = new Point(0, 2);
+		Rectangle r = new Rectangle(p1);
+		r.add(p2);
+		System.out.println(r.getBounds());
 	}
     public Rectangle createRectangle(Point x, Point y) {
-	    int width = (int) Math.abs(y.getX() - x.getX());
-	    int height = (int) Math.abs(y.getY() - x.getY());
-	    return new Rectangle(x, new Dimension(width, height));
+	    Rectangle r = new Rectangle(x);
+	    r.add(y);
+	    return r;
     }
 }
