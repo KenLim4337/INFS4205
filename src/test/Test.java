e5219fd5becbf022e51f0f626ab51aa2bbd11b58
KenLim4337 <lkyoong428@gmail.com>
2017-04-16 16:47:09 +1000

    Basic test case, input reading framework

--- src/test/Test.java
+++ src/test/Test.java
@@ -1,6 +1,9 @@
 package test;
 
-import java.awt.*;
+import java.awt.Rectangle;
+import java.awt.Point;
+import java.io.*;
+import java.util.*;
 
 public class Test {
 	public static void main(String [] args) {
@@ -10,10 +13,41 @@ public class Test {
 		Rectangle r = new Rectangle(p1);
 		r.add(p2);
 		System.out.println(r.getBounds());
+		
+		//Reading test code
+		readFile("");
 	}
-    public Rectangle createRectangle(Point x, Point y) {
+    
+	
+	public Rectangle createRectangle(Point x, Point y) {
 	    Rectangle r = new Rectangle(x);
 	    r.add(y);
 	    return r;
+    }
+    
+	/*File reading*/
+    public static Map<Integer, int[]> readFile(String filename) {
+    	
+    	Map<Integer, int[]> result = new HashMap<Integer, int[]>();
+    	
+    	try {
+    		File file = new File (filename);
+    		BufferedReader reader = new BufferedReader(new FileReader(file));
+    		System.out.println("\nFile loaded!");
+    		
+    		String line=null;
+    	    while((line=reader.readLine()) != null) {
+    	    	System.out.println(line);
+    	    }
+    	    
+    	    
+    	} catch (IOException e) {
+    		System.out.print("I/O Exception: " + e.getMessage());
+    	} 
+    	
+    	
+    	
+    	
+    	return result;
     }
 }
